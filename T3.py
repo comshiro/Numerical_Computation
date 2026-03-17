12b194bd335882ed5bebdb7477222d1a46cc2f4a
anamunteanu2004 <79170424+anamunteanu2004@users.noreply.github.com>
2026-03-17 23:04:44 +0200

    Tema 3

---
+++ T3.py
@@ -0,0 +1,149 @@
+import numpy as np
+
+##EXERCITIUL 1##
+def calculeaza_vector_b(A, s, n):
+    b = np.zeros(n)
+    for i in range(n):
+        suma = 0
+        for j in range(n):
+            suma += s[j] * A[i][j]
+        b[i] = suma
+    return b
+
+##EXERCITIUL 2##
+def descompunere_qr_householder(A):
+    n = A.shape[0]
+    R = A.copy().astype(float)
+    Q = np.eye(n)
+
+    for k in range(n - 1):
+        x = R[k:, k]
+
+        norm_x = np.linalg.norm(x)
+        if norm_x == 0:
+            continue
+
+        semn = 1 if x[0] >= 0 else -1
+        alfa = semn * norm_x
+
+        u = x.copy()
+        u[0] += alfa
+        norm_u = np.linalg.norm(u)
+
+        if norm_u == 0:
+            continue
+
+        v = u / norm_u
+        v = v.reshape(-1, 1)
+
+        H_k = np.eye(n - k) - 2 * (v @ v.T)
+        H_n = np.eye(n)
+        H_n[k:, k:] = H_k
+        R = H_n @ R
+        Q = Q @ H_n
+
+    return Q, R
+
+##EXERCITIUL 3##
+def substitutie_inapoi(R, y):
+    n = R.shape[0]
+    x = np.zeros(n)
+
+    for i in range(n - 1, -1, -1):
+        suma_cunoscuta = 0
+        for j in range(i + 1, n):
+            suma_cunoscuta += R[i, j] * x[j]
+
+        if R[i, i] == 0:
+            raise ValueError("Matricea R are un zero pe diagonala. Sistemul nu are solutie unica.")
+        x[i] = (y[i] - suma_cunoscuta) / R[i, i]
+
+    return x
+
+
+def rezolva_sistem_qr(Q, R, b):
+    y = Q.T @ b
+    return substitutie_inapoi(R, y)
+
+
+def exercitiul_3(A, b):
+    Q_np, R_np = np.linalg.qr(A)
+    x_QR = rezolva_sistem_qr(Q_np, R_np, b)
+
+    Q_h, R_h = descompunere_qr_householder(A)
+    x_Householder = rezolva_sistem_qr(Q_h, R_h, b)
+    norma_diferentei = np.linalg.norm(x_QR - x_Householder, ord=2)
+
+    return x_QR, x_Householder, norma_diferentei
+
+##EXERCITIUL 4##
+def exercitiul_4(A_init, b_init, x_householder, x_QR, s):
+    norm_s = np.linalg.norm(s, ord=2)
+
+    err_rezidual_house = np.linalg.norm(A_init @ x_householder - b_init, ord=2)
+    err_rezidual_qr = np.linalg.norm(A_init @ x_QR - b_init, ord=2)
+
+    err_relativ_house = np.linalg.norm(x_householder - s, ord=2) / norm_s
+    err_relativ_qr = np.linalg.norm(x_QR - s, ord=2) / norm_s
+
+    return err_rezidual_house, err_rezidual_qr, err_relativ_house, err_relativ_qr
+
+##EXERCITIUL 5##
+def calculeaza_inversa_qr(Q, R):
+    n = R.shape[0]
+    A_inv = np.zeros((n, n))
+    I = np.eye(n)
+
+    for i in range(n):
+        e_i = I[:, i]
+        coloana_inversa = rezolva_sistem_qr(Q, R, e_i)
+        A_inv[:, i] = coloana_inversa
+
+    return A_inv
+
+
+def exercitiul_5(A, Q, R):
+    A_inv_householder = calculeaza_inversa_qr(Q, R)
+    A_inv_bibl = np.linalg.inv(A)
+
+    norma_diferentei = np.linalg.norm(A_inv_householder - A_inv_bibl)
+
+    return A_inv_householder, A_inv_bibl, norma_diferentei
+
+##EXERCITIUL 6##
+def exercitiul_6_test_random(n):
+    print(f"\n{'=' * 50}")
+    print(f"--- testam pentru dimensiunea n = {n} ---")
+    print(f"{'=' * 50}")
+
+    A_rand = np.random.uniform(-10, 10, (n, n))
+    s_rand = np.random.uniform(-10, 10, n)
+
+    b_rand = calculeaza_vector_b(A_rand, s_rand, n)
+
+    Q_h, R_h = descompunere_qr_householder(A_rand)
+    x_QR, x_Householder, eroare_ex3 = exercitiul_3(A_rand, b_rand)
+
+    e_rez_h, e_rez_qr, e_rel_h, e_rel_qr = exercitiul_4(A_rand, b_rand, x_Householder, x_QR, s_rand)
+
+    A_inv_H, A_inv_np, eroare_inv = exercitiul_5(A_rand, Q_h, R_h)
+
+    print("\nRezultatele(erori obtinute):")
+    print(f"Eroare reziduala Householder: {e_rez_h:.2e}")
+    print(f"Eroare reziduala NumPy:       {e_rez_qr:.2e}")
+    print(f"Eroare relativa Householder:  {e_rel_h:.2e}")
+    print(f"Eroare relativa NumPy:        {e_rel_qr:.2e}")
+    print(f"Eroare la calculul inversei:  {eroare_inv:.2e}")
+
+    limita = 10 ** (-6)
+    toate_erorile = [e_rez_h, e_rez_qr, e_rel_h, e_rel_qr, eroare_inv]
+
+    if all(eroare < limita for eroare in toate_erorile):
+        print(f"\nV Toate erorile pentru n={n} au ramas sub 10^-6.")
+    else:
+        print(f"\npentru n={n}, limitarile procesorului incep sa se simta.")
+
+if __name__ == "__main__":
+    exercitiul_6_test_random(5)
+    exercitiul_6_test_random(100)
+    
